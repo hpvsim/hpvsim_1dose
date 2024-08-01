fcebb2628d453b90f71c2341c7dbeea1c00a1feb
Robyn Stuart <robyna.s@gmail.com>
2024-08-01 14:31:18 +1000

    plot calibrations

---
+++ plot_figS1.py
@@ -0,0 +1,86 @@
+"""
+Plot calibrations
+"""
+
+# Import packages
+import sciris as sc
+import numpy as np
+import pylab as pl
+import pandas as pd
+import seaborn as sns
+
+
+# Imports from this repository
+import locations as loc
+import utils as ut
+
+
+#%% Plotting functions
+def plot_fig1(locations, filestem='', n_results=50):
+
+    ut.set_font(12)
+    n_plots = len(locations)
+    fig, axes = sc.getrowscols(n_plots, make=True, remove_extra=True, figsize=(25,12))
+    axes = axes.flatten()
+    resname = 'cancers'
+    plot_count = 0
+    date = 2020
+
+    for pn, location in enumerate(locations):
+
+        # Plot settings
+        ax = axes[plot_count]
+
+        dflocation = location.replace(' ', '_')
+        calib = sc.loadobj(f'results/{dflocation}_calib{filestem}_reduced.obj')
+        reslist = calib.analyzer_results
+        target_data = calib.target_data[0]
+        target_data = target_data[(target_data.name == resname)]
+
+        # Make labels
+        baseres = reslist[0]['cancers']
+        age_labels = [str(int(baseres['bins'][i])) + '-' + str(int(baseres['bins'][i + 1])) for i in range(len(baseres['bins'])-1)]
+        age_labels.append(str(int(baseres['bins'][-1])) + '+')
+        res = reslist
+
+        # Plot data
+        x = np.arange(len(age_labels))
+        ydata = np.array(target_data.value)
+        ax.scatter(x, ydata, color='k', marker='s', label='Data')
+
+        # Construct a dataframe with things in the most logical order for plotting
+        bins = []
+        values = []
+        for run_num, run in enumerate(res):
+            bins += x.tolist()
+            values += list(run[resname][date])
+        modeldf = pd.DataFrame({'bins': bins, 'values': values})
+        sns.boxplot(ax=ax, x='bins', y='values', data=modeldf, color='b', boxprops=dict(alpha=.4))
+
+        # Set title and labels
+        title_country = location.title()
+        if title_country == 'Cote Divoire':
+            title_country = "Cote d'Ivoire"
+        ax.set_title(title_country)
+        ax.set_ylabel('')
+        ax.set_xlabel('')
+        if pn in [0, 5, 10, 15, 20, 25]:
+            ax.set_ylabel('# cancers')
+        if pn in [25, 26, 27, 28, 29]:
+            stride = np.arange(0, len(baseres['bins']), 2)
+            ax.set_xticks(x[stride], baseres['bins'].astype(int)[stride])
+        else:
+            ax.set_xticks(x, [])
+        plot_count += 1
+
+    fig.tight_layout()
+    pl.savefig(f"figures/fig1.png", dpi=100)
+
+
+#%% Run as a script
+if __name__ == '__main__':
+
+    locations = loc.locations
+    plot_fig1(locations)
+
+    print('Done.')
---
+++ plot_figS2.py
@@ -0,0 +1,66 @@
+"""
+Plot ASR
+"""
+import hpvsim as hpv
+import pylab as pl
+import pandas as pd
+import numpy as np
+import sciris as sc
+import utils as ut
+import seaborn as sns
+from matplotlib.ticker import FormatStrFormatter
+
+
+
+# Imports from this repository
+import locations as loc
+import utils as ut
+
+
+#%% Plotting functions
+def plot_fig2(locations, filestem='', n_results=50):
+
+    ut.set_font(14)
+    colors = sc.gridcolors(2)
+
+    n_plots = len(locations)
+    fig, axes = sc.getrowscols(n_plots, make=True, remove_extra=True, figsize=(25,12))
+    axes = axes.flatten()
+    resname = 'asr_cancer_incidence'
+    plot_count = 0
+    date = 2020
+
+    for pn, location in enumerate(locations):
+
+        # Plot settings
+        ax = axes[plot_count]
+        dflocation = location.replace(' ', '_')
+        res = sc.loadobj(f'raw_results/{dflocation}.mres')
+        data = pd.read_csv(f'data/{dflocation}_asr_cancer_incidence.csv')
+
+        start_year = 2000
+        ind = sc.findinds(res['year'], start_year)[0]
+        years = res['year'][ind:]
+
+        ax.plot(years, res[resname].values[ind:], color=colors[0], label=f'HPVsim')
+        ax.fill_between(years, res[resname].low[ind:],
+                                res[resname].high[ind:], color=colors[0], alpha=0.3)
+
+        ax.plot(2020, data['value'].values[0], marker='s', color=colors[1], label='Globocan')
+        ax.set_ylabel('ASR incidence (per 100k)')
+        ax.legend()
+        ax.set_ylim(bottom=0)
+
+        plot_count += 1
+
+    fig.tight_layout()
+    pl.savefig(f"figures/fig2.png", dpi=100)
+
+
+
+# %% Run as a script
+if __name__ == '__main__':
+
+    plot_fig2(loc.locations)
+
+    print('Done.') 
