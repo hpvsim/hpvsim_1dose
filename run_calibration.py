328cd2514d4dbdede6cdb3e9aed8e1ad037b24cc
Robyn Stuart <robyna.s@gmail.com>
2025-06-13 08:25:54 +1000

    remove storage

--- run_calibration.py
+++ run_calibration.py
@@ -33,8 +33,8 @@ locations = loc.locations
 # Run settings for calibration (dependent on debug)
 n_trials = [1000, 1][debug]  # How many trials to run for calibration
 n_workers = [40, 1][debug]  # How many cores to use
-storage = ["mysql://hpvsim_user@localhost/hpvsim_db", None][debug]  # Storage for calibrations
-
+# storage = ["mysql://hpvsim_user@localhost/hpvsim_db", None][debug]  # Storage for calibrations
+storage = None
 
 ########################################################################
 # Run calibration
