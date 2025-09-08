eb1c82bc0484657ef67d88a881b59a4baa315e6c
Robyn Stuart <robyna.s@gmail.com>
2025-09-08 11:56:48 +1000

    run

--- run_scenarios.py
+++ run_scenarios.py
@@ -198,8 +198,8 @@ def run_sims(location=None, calib_pars=None, vx_scenarios=None, end=2100, verbos
 if __name__ == '__main__':
 
     T = sc.timer()
-    do_run = False
-    do_process = False
+    do_run = True
+    do_process = True
     do_compile = True
     end = 2125
 
